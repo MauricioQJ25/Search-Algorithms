231e6fe60045cc20bed6a576245a09a8d79d864a
Maukrack <majiqtresi@gmail.com>
2021-09-06 21:38:43 -0500

    Adding adjacencylist to main

---
+++ adjacencyList.h
@@ -0,0 +1,29 @@
+/*******************************************/
+/* Initial Version 0.0.1                   */
+/* Created by Mauricio Quijada             */
+/* Email mauricio.quijada@outlook.com      */
+/* Homework 01 Artificial Intelligence     */
+/* Universidad Nacional Autonoma de Mexico */
+/*                                         */
+/* adjacencyList.h                         */
+/* This program represents a Graph G(V,E)  */
+/* in a adjacency List                     */
+/*******************************************/
+
+
+
+/**********************************************/
+/* Adding new node given the following inputs */
+/* s--source                                  */
+/* d--destiny                                 */
+/* w--weight                                  */
+/* Head weight is filled with -1              */
+/**********************************************/
+void addNode(int s, int d, int w);
+
+/**********************************************/
+/* Printing a full filled list                */
+/**********************************************/
+void printList();
+
+void initAdjacencyList();
